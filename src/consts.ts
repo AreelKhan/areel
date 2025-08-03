6bf90ebb8759082b9cd07a05d40c3f9121927dbe
Areel Khan <areel@fablesecurity.com>
2025-08-03 11:55:47 -0400

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,19 @@
 import type { Metadata, Site } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro Academics",
-  DESCRIPTION: "Astro Micro Academics is for academic user.",
-  EMAIL: "youremial@gmail.com",
+  TITLE: "Areel Khan",
+  DESCRIPTION: "Areel Khan's personal website.",
+  EMAIL: "areel.khan.career@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 2,
   NUM_PUBLICATIONS_ON_HOMEPAGE: 3,
   SITEURL: 'https://astro-micro-academic.vercel.app' // Update here to link the RSS icon to your website rss
 };
 
-export const HIGHLIGHTAUTHOR = "John B"
+export const HIGHLIGHTAUTHOR = "Areel Khan"
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "Areel Khan's personal website.",
 };
 
 export const BLOG: Metadata = {
@@ -21,26 +21,18 @@ export const BLOG: Metadata = {
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
-export const RESEARCH: Metadata = {
-  TITLE: "Publications",
-  DESCRIPTION:
-    "A collection of my publications with links to paper, repositories and live demos.",
+export const EXPERIENCE: Metadata = {
+  TITLE: "Experience",
+  DESCRIPTION: "Areel Khan's experience.",
 };
 
-export const CV: Metadata = {
-  TITLE: "CV",
-  DESCRIPTION:
-    "your cv",
+export const RESUME: Metadata = {
+  TITLE: "Resume",
+  DESCRIPTION: "Areel Khan's resume.",
 };
 
 export const TAGS: Metadata = {
   TITLE: "TAGS",
   DESCRIPTION:
     "blog tag filter",
-};
-
-export const ABOUT: Metadata = {
-  TITLE: "ABOUT",
-  DESCRIPTION:
-    "A self-intro",
 };
